7df88266681bbe932195f18b6441b0361cf185b2
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-12-22 11:39:58 +0530

    ch2: templates

--- app.py
+++ app.py
@@ -1,32 +1,24 @@
-from markupsafe import escape
-from flask import Flask, abort
+import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-@app.route('/index/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 @app.route('/about/')
 def about():
-    return '<h3>This is a Flask web application.</h3>'
+    return render_template('about.html')
 
-@app.route('/capitalize/<word>/')
-def capitalize(word):
-    return '<h1>{}</h1>'.format(escape(word.capitalize()))
 
-@app.route('/add/<int:n1>/<int:n2>/')
-def add(n1, n2):
-    return '<h1>{}</h1>'.format(n1 + n2)
+@app.route('/comments/')
+def comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.'
+                ]
 
-@app.route('/users/<int:user_id>/')
-def greet_user(user_id):
-    users = ['Bob', 'Jane', 'Adam']
-    try:
-        return '<h2>Hi {}</h2>'.format(users[user_id])
-    except IndexError:
-        abort(404)  
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('comments.html', comments=comments)
